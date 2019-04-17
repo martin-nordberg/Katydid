bbcf77287b458e3bcf7a4a71e72d08b504ec58a2
martin-nordberg <martin.nordberg@verizon.net>
2019-04-17 08:46:07 -0400

    Changed the symbolic links to make the JS subsystems primary (for simpler sharing with client projects). - Step 1: Removed existing symlinks.

---
+++ MakeLinks.bat
@@ -0,0 +1,22 @@
+
+rmdir Katydid-VDOM-JVM\src\main\kotlin\o
+mklink /d Katydid-VDOM-JVM\src\main\kotlin\o ..\..\..\..\Katydid-VDOM-JS\src\main\kotlin\o
+
+rmdir Katydid-VDOM-JVM\src\main\kotlin\i
+mklink /d Katydid-VDOM-JVM\src\main\kotlin\i ..\..\..\..\Katydid-VDOM-JS\src\main\kotlin\i
+
+
+rmdir Katydid-CSS-JVM\src\main\kotlin\o
+mklink /d Katydid-CSS-JVM\src\main\kotlin\o ..\..\..\..\Katydid-CSS-JS\src\main\kotlin\o
+
+rmdir Katydid-CSS-JVM\src\main\kotlin\i
+mklink /d Katydid-CSS-JVM\src\main\kotlin\i ..\..\..\..\Katydid-CSS-JS\src\main\kotlin\i
+
+
+rmdir Katydid-Events-JVM\src\main\kotlin\o
+mklink /d Katydid-Events-JVM\src\main\kotlin\o ..\..\..\..\Katydid-Events-JS\src\main\kotlin\o
+
+rmdir Katydid-Events-JVM\src\main\kotlin\i
+mklink /d Katydid-Events-JVM\src\main\kotlin\i ..\..\..\..\Katydid-Events-JS\src\main\kotlin\i
+
+
